58b7e2f017b4c6af59bb1623d1734a73a047f7ff
rafa238 <rafalaureano246@gmail.com>
2022-12-07 22:57:38 -0600

    disjoint set added

---
+++ disjoint_set.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+class UnionFind {
+    //p -> parent respectivly
+    //rank -> height of each conjunt
+    private: vi p, rank;
+    public:
+        UnionFind(int N){
+            rank.assign(N,0);
+            p.assign(N, 0);
+            for(int i=0; i<N; i++){
+                p[i] = i;
+            }
+        }
+        int findSet(int i){
+            return (p[i] == i) ? i : (p[i] = findSet(p[i]));
+        }
+        bool isSameSet(int i, int j){
+            return findSet(i) == findSet(j);
+        }
+        void unionSet(int i, int j){
+            if(!isSameSet(i, j)){
+                int x = findSet(i) , y = findSet(j);
+                if(rank[x] > rank[y]) p[y] = x;
+                else {
+                    p[x] = y;
+                    if(rank[x] == rank[y]) rank[y]++;
+                }
+            }
+        }
+};
